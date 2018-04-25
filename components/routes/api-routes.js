7062aacb8818f34ea3eaa3e6c55eb2a4397f9264
feliun <felipe.polo@gmail.com>
2018-04-25 18:34:01 +0200

    Receiving savedsearch request

--- components/routes/api-routes.js
+++ components/routes/api-routes.js
@@ -1,9 +1,11 @@
+const bodyParser = require('body-parser');
 const express = require('systemic-express/express');
 const { join } = require('path');
 
 module.exports = () => {
   const start = ({ app }, cb) => {
     app.use(express.static('public'));
+    app.use(bodyParser.json());
 
     app.get('/tes', (req, res) => {
       res.sendFile(join(process.cwd(), 'public', 'views', 'tes.html'));
@@ -14,6 +16,11 @@ module.exports = () => {
         id: process.env.BOT_ID,
         buttonColor: process.env.BOT_COLOR || '#0000ff',
       });
+    });
+
+    app.post('/savedsearch', (req, res) => {
+      // console.log(req.body);
+      res.json({ success: true });
     });
 
     cb();
