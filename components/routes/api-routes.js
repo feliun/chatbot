be324dfcbce5a069750c9e133bf6315c0519a284
feliun <felipe.polo@gmail.com>
2018-05-01 12:20:46 +0200

    Adding support for more bots

--- components/routes/api-routes.js
+++ components/routes/api-routes.js
@@ -6,6 +6,7 @@ const { join } = require('path');
 module.exports = () => {
   const start = ({ app, collections }, cb) => {
     app.use(express.static('public'));
+    app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
 
     const format = (date) => moment(date).format('DD-MM-YYYYTHH:mm:ss');
@@ -14,29 +15,40 @@ module.exports = () => {
       res.sendFile(join(process.cwd(), 'public', 'views', 'tes.html'));
     });
 
+    app.get('/yoigo', (req, res) => {
+      res.sendFile(join(process.cwd(), 'public', 'views', 'yoigo.html'));
+    });
+
     app.get('/bot', (req, res) => {
+      const { company }  = req.query || 'tes';
+      const idByCompany = {
+        tes: process.env.TES_BOT_ID,
+        yoigo: process.env.YOIGO_BOT_ID,
+      };
       res.json({
-        id: process.env.BOT_ID,
+        id: idByCompany[company],
         buttonColor: process.env.BOT_COLOR || '#0000ff',
       });
     });
 
     app.post('/savedsearch', (req, res) => {
-      const email = 'felipe.polo@guidesmiths.com';
+      const email = req.body.reply;
       collections.saved_search.findOne({ 'user.email': email })
-        .then(({ user, sentDate }) => {
+      .then(({ user, sentDate }) => {
           const { firstName, lastName, displayName } = user;
           res.json({
             sentDate: format(sentDate),
             firstName,
             lastName,
             displayName,
+            email,
           });
         });
     });
 
     app.post('/blacklist/check', (req, res) => {
-      const email = 'felipe.polo@guidesmiths.com';
+      const TARGET_TASK = '13482';
+      const { email } = req.body.customVariables[TARGET_TASK];
       collections.blacklist.findOne({ email })
         .then((result) => {
           if (!result) return res.json({ found: false });
